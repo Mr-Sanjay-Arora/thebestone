4bb2f6bc59ba236d8c2c5fcc0de7314384c05fd8
Sanjay Arora <i.arorasunny@gmail.com>
2020-11-20 01:03:30 +0530

    Pages Updated
    Need some more time to make the website responsive for mobile users.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -15,7 +15,29 @@ $('.cart-browse').on('click',function(){
     arrows: true,
   });
 
+$('.store-categories-inner').slick({
+    infinite: true,
+    autoplay: true,
+    slidesToShow: 10,
+    slidesToScroll: 6,
+    speed: 3000,
+    arrows: true,
+  });
+
+$('.product-image-slider').slick({
+    infinite: true,
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 500,
+    dots: true
+  });
+
 $('.more-category').on('click',function(){
     $('.more-category').toggleClass('show');
     $('.list-more-category').toggleClass('hide');
+})
+
+$('.close-tooltip').on('click',function(){
+    $('.location-tooltip').css('display','none');
 })
